398f688a664b2424e641c309620a1f0a391bbca5
Paul Schroeder (external expert on behalf of DB Netz) <paul.schroeder-extern@deutschebahn.com>
2023-03-03 16:34:54 +0100

    wip
    Signed-off-by: Paul Schroeder (external expert on behalf of DB Netz) <paul.schroeder-extern@deutschebahn.com>

--- pkg/controller/rds/dbcluster/setup_test.go
+++ pkg/controller/rds/dbcluster/setup_test.go
@@ -186,3 +186,160 @@ func TestIsVPCSecurityGroupIDsUpToDate(t *testing.T) {
 		})
 	}
 }
+
+func TestIsEngineVersionUpToDate(t *testing.T) {
+	type args struct {
+		cr  *svcapitypes.DBCluster
+		out *svcsdk.DescribeDBClustersOutput
+	}
+
+	type want struct {
+		isUpToDate bool
+	}
+
+	cases := map[string]struct {
+		args
+		want
+	}{
+		"Nil": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							EngineVersion: nil,
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{},
+				},
+			},
+			want: want{
+				isUpToDate: false,
+			},
+		},
+		"DesiredBeingManged": { // AWS default or managed by DBCluster
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: nil,
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: true,
+			},
+		},
+		"ActualEmpty": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: nil,
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: false,
+			},
+		},
+		"Unsorted": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: true,
+			},
+		},
+		"Identical": {
+			args: args{
+				cr: &svcapitypes.DBCluster{
+					Spec: svcapitypes.DBClusterSpec{
+						ForProvider: svcapitypes.DBClusterParameters{
+							CustomDBClusterParameters: svcapitypes.CustomDBClusterParameters{
+								VPCSecurityGroupIDs: []string{"sg-123", "sg-456"},
+							},
+						},
+					},
+				},
+				out: &svcsdk.DescribeDBClustersOutput{
+					DBClusters: []*svcsdk.DBCluster{
+						{
+							VpcSecurityGroups: []*svcsdk.VpcSecurityGroupMembership{
+								{
+									VpcSecurityGroupId: ptr("sg-123"),
+								},
+								{
+									VpcSecurityGroupId: ptr("sg-456"),
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				isUpToDate: true,
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			isUpToDate := areVPCSecurityGroupIDsUpToDate(tc.args.cr, tc.args.out)
+
+			if diff := cmp.Diff(tc.want.isUpToDate, isUpToDate); diff != "" {
+				t.Errorf("r: -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
